7b2934fc57b30f17c0d1f21e0a5a534c9ef43dc7
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-25 21:33:34 +0530

    :sparkles: Firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:color_log/color_log.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:redit_clone_flutter/features/auth/screens/login_screen.dart';
 import 'package:redit_clone_flutter/theme/pallete.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  try {
+    await Firebase.initializeApp();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    clog.info("Firebase setup successfully");
+  } catch (e) {
+    clog.error(e.toString());
+  }
 
   runApp(const MyApp());
 }
