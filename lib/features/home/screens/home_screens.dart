58b74a1aa2f3c6ac70aa71252a40bf00d615fa59
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-26 15:01:01 +0530

    :zap: App setUp

--- lib/features/home/screens/home_screens.dart
+++ lib/features/home/screens/home_screens.dart
@@ -9,8 +9,29 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var user = ref.watch(userProvider);
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: false,
+        leading: IconButton(
+          splashRadius: 25,
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+              splashRadius: 25,
+              onPressed: () {},
+              icon: const Icon(Icons.search)),
+          IconButton(
+              splashRadius: 25,
+              onPressed: () {},
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(user!.profilePic!),
+              ))
+        ],
+      ),
       body: Center(
-        child: Text(user?.name ?? "xxxxxxx"),
+        child: Text(user.name ?? "xxxxxxx"),
       ),
     );
   }
